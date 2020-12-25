5cfaa856a6a105ef3c977e33b6aafbc8131431f8
Andy Frievalt <afrievalt@yahoo.com>
2020-12-24 21:42:39 -0600

    added thunk

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import SignUpForm from './pages/SignUpForm';
 import {Provider} from 'react-redux'
-import store from './store/createStore';
+import store, { rrfProps } from './store/createStore';
 import {ReactReduxFirebaseProvider, useFirebaseConnect} from 'react-redux-firebase'
-import firebase from 'firebase/app'
-import firebaseConfig from './store/firebaseConfig';
 import FirebaseListener from './controls/FirebaseListener';
 
-const rrfProps = {
-  firebase,
-  config: firebaseConfig,
-  dispatch: store.dispatch
-  // createFirestoreInstance // <- needed if using firestore
-}
-
-//move to firebase actions???
-firebase.initializeApp(firebaseConfig);
 
 function App() {
-
-
   return (
     <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,27 +1,40 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import { connectRoutes } from 'redux-first-router';
-
 import rootReducer from './rootReducer'
 import routePaths from './routePaths';
+import firebase from 'firebase/app'
+import firebaseConfig from './firebaseConfig';
+import { getFirebase } from 'react-redux-firebase'
 
 const {middleware: routerMiddleware, enhancer: routerEnhancer, initialDispatch} = connectRoutes(routePaths, {initialDispatch: false});
 
-// const store = configureStore({ reducer: rootReducer }) The store now has
-// redux-thunk added and the Redux DevTools Extension is turned on
+firebase.initializeApp(firebaseConfig);
 
-
-
+const defaultMiddleware = getDefaultMiddleware({
+  thunk: {
+    extraArgument: getFirebase,
+  },
+  serializableCheck: false,
+})
 
 
 function configureRouteStore() {
   const store = configureStore({
     reducer: rootReducer,
-    middleware: [...getDefaultMiddleware(), routerMiddleware],
+    middleware: [...defaultMiddleware, routerMiddleware],
     enhancers: (defaultEnhancers) => [routerEnhancer, ...defaultEnhancers]
   })
   initialDispatch();
   return store;
 }
-
 const store = configureRouteStore();
+
+
+export const rrfProps = {
+  firebase,
+  config: firebaseConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
+
 export default store;
