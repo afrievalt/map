fb9eb7ff1efbdaeca774ae9e65b776d397fb4d1f
Andy Frievalt <afrievalt@yahoo.com>
2020-12-27 07:50:12 -0600

    plop fix duck

--- plopfile.js
+++ plopfile.js
@@ -1,59 +1,23 @@
-module.exports = function (plop) {
-	// controller generator
-	plop.setGenerator('field', {
-		description: 'create a field control to be used in react final forms',
-		prompts: [
-			{
-				type: 'input',
-				name: 'name',
-				message: 'field name?',
-			},
-		],
-		actions: [
-			{
-				type: 'add',
-				path: 'src/app/fields/{{name}}.js',
-				templateFile: 'plop-templates/InputField.js.hbs',
-			},
-		],
-	});
+const _duck = require('./plop-templates/_duck')
 
-	plop.setGenerator('duck', {
-		description: 'create a redux slice with redux-toolkit',
-		prompts: [
-			{
-				type: 'input',
-				name: 'name',
-				message: 'duck name?',
-			},
-		],
-		actions: [
-			{
-				type: 'add',
-				path: 'src/app/store/ducks/{{name}}.duck.js',
-				templateFile: 'plop-templates/Duck.js.hbs',
-			},
-			{
-				type: 'add',
-				path: 'src/app/store/ducks/{{name}}.duck.spec.js',
-				templateFile: 'plop-templates/Duck.spec.js.hbs',
-			},
-			{
-				// Action type 'append' injects a template into an existing file
-				type: 'append',
-				path: 'src/app/store/root-reducer.js',
-				// Pattern tells plop where in the file to inject the template
-				pattern: `import { combineReducers } from 'redux';`,
-				template: `import {{name}} from './ducks/{{name}}.duck';`,
-			},
-			{
-				// Action type 'append' injects a template into an existing file
-				type: 'append',
-				path: 'src/app/store/root-reducer.js',
-				// Pattern tells plop where in the file to inject the template
-				pattern: `auth,`,
-				template: `{{name}},`,
-			},
-		],
-	});
-};
+module.exports = function (plop) {
+  // controller generator
+  plop.setGenerator('field', {
+    description: 'create a field control to be used in react final forms',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'field name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/app/fields/{{name}}.js',
+        templateFile: 'plop-templates/InputField.js.hbs'
+      }
+    ]
+  })
+  plop.setGenerator('duck', _duck)
+}
