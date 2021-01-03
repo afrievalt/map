abfadc88c2f9c726af2e9d7874f59420dfa860cf
Andy Frievalt <afrievalt@yahoo.com>
2021-01-03 07:35:15 -0600

    plop add FieldGroup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import { Container } from '@material-ui/core'
 import React from 'react'
 import WelcomeSection from '../../controls/WelcomeSection/WelcomeSection'
 import AddStudentForm from '../AddStudentForm/AddStudent.form'
-
 const Home = () => {
   return (
     <Container>
