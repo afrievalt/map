3873765b58493fb1c73d284489a9c394585ce4e6
Andy Frievalt <afrievalt@yahoo.com>
2021-01-08 08:13:50 -0600

    plop add path to add form

--- plop-templates/_page.js
+++ plop-templates/_page.js
@@ -16,12 +16,23 @@ module.exports = {
       message: '{{type}} name?'
     }
   ],
-  actions: [
-    {
-      type: 'addMany',
-      destination: 'src/pages/{{name}}Form',
-      base: 'plop-templates/page-templates/{{type}}-templates',
-      templateFiles: 'plop-templates/page-templates/{{type}}-templates/*.hbs'
-    }
-  ]
+  actions: data => {
+    const actions = [
+      {
+        type: 'addMany',
+        destination: 'src/pages/{{name}}Form',
+        base: 'plop-templates/page-templates/{{type}}-templates',
+        templateFiles: 'plop-templates/page-templates/{{type}}-templates/*.hbs'
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/store/rootReducer.js',
+        // Pattern tells plop where in the file to inject the template
+        pattern: "HOME: '/',",
+        template: "ADD_CONTACT: '/contact/add',\nEDIT_CONTACT: '/contact/:contactId',"
+      }
+    ]
+    return actions
+  }
 }
