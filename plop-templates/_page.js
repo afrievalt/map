e1032d09b7256262af6b08e0bbdb321ecd443bd1
Andy Frievalt <afrievalt@yahoo.com>
2021-01-15 21:06:19 -0600

    tix path

--- plop-templates/_page.js
+++ plop-templates/_page.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       {
         type: 'modify',
-        path: 'src/controls/LefSidebar/LeftDrawer.jsx',
+        path: 'src/controls/LeftSidebar/LeftDrawer.jsx',
         // Pattern tells plop where in the file to inject the template
         pattern: '</List>',
         templateFile: 'plop-templates/fragment-templates/page/{{lowerCase name}}/LeftDrawer.jsx.hbs'
