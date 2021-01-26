5aa189462dfb8278fbae229599d50c343185fe48
Andy Frievalt <afrievalt@yahoo.com>
2021-01-26 14:27:07 -0600

    add titles

--- src/routes/Title.jsx
+++ src/routes/Title.jsx
@@ -2,9 +2,16 @@ import Typography from '@material-ui/core/Typography'
 import { useSelector } from 'react-redux'
 
 const lookupTitle = {
-  HOME: 'Home'
+  HOME: 'Home',
+  COMPANY_ADD: 'Add Company',
+  COMPANY_EDIT: 'Edit Company',
+  COMPANY: 'Company',
+  CONTACT_ADD: 'Add Contact',
+  CONTACT_EDIT: 'Edit Contact',
+  CONTACT: 'Contact',
+  SIGNUP: 'Sign Up',
+  SIGNIN: 'Sign In'
 }
-
 const Title = () => {
   const locationType = useSelector(state => state.location.type)
   const title = lookupTitle[locationType]
