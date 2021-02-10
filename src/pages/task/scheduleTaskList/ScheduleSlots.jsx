92b741b9c31c534618eb4f9299ada399376c48ba
Andy Frievalt <afrievalt@yahoo.com>
2021-02-10 06:36:01 -0600

    Added drop target

--- src/pages/task/scheduleTaskList/ScheduleSlots.jsx
+++ src/pages/task/scheduleTaskList/ScheduleSlots.jsx
@@ -1,52 +1,23 @@
 import { selectTaskListData } from './taskList.selectors.js'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemText from '@material-ui/core/ListItemText'
-import Checkbox from '@material-ui/core/Checkbox'
-import IconButton from '@material-ui/core/IconButton'
-import CommentIcon from '@material-ui/icons/Comment'
 import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
 import { toggleTaskStatus } from './taskActions.js'
 import { useActions } from '../../../hooks/useActions.js'
-import { Draggable } from 'react-beautiful-dnd'
+
+import ListSubheader from '@material-ui/core/ListSubheader'
 
 const TaskList = (provider) => {
   const update = useActions(toggleTaskStatus)
-  const handleClickCheckbox = (id, isChecked) => () => {
-    update(id, isChecked)
-  }
   const taskList = useSelector(selectTaskListData)
   return (
 
     <List {...provider.droppableProps}>
-      {taskList.map(({ title, id, checked, indeterminate, color }, i) => {
-        const labelId = `checkbox-list-label-${id}`
-        return (
-          <Draggable key={id} draggableId={id} index={i}>
-            {(provided) => (
-              <>
-                {provided.placeholder}
-                <ListItem
-                  ContainerProps={{ ref: provided.innerRef }}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                  role={undefined}
-                  dense
-                  button
-                  onClick={handleClickCheckbox(id)}
-                >
-                  <ListItemText id={labelId} primary={title} />                  
-              </>
-            )}
-          </Draggable>
-        )
-      })}
-      {provider.placeholder}
+      <ListSubheader>Drop here 1</ListSubheader>
+      <ListSubheader>Drop here 2</ListSubheader>
     </List>
   )
+
 }
 
 export default memo(TaskList)
--- src/pages/task/scheduleTaskList/ScheduleTask.list.jsx
+++ src/pages/task/scheduleTaskList/ScheduleTask.list.jsx
@@ -5,23 +5,33 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import TaskList from './Task.list.jsx'
 import { useActions } from '../../../hooks/useActions.js'
 import { dragEnd } from './taskActions.js'
-
+import ScheduleSlots from './ScheduleSlots.jsx'
 
 const ScheduleTaskList = () => {
+  // const handleDragEnd = (foo) => {
+  //   console.log('ScheduleTask.list.jsx:13 foo: ', foo)
+  // }
   const handleDragEnd = useActions(dragEnd)
+
   return (
     <Container>
       <MiniTaskForm />
       <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId='schedule'>
+          {(provided) => (
+            <div ref={provided.innerRef}>
+              <ScheduleSlots />
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
         <Droppable droppableId='backlog'>
           {(provided) => (
             <div ref={provided.innerRef}>
               <TaskList provided={provided} />
               {provided.placeholder}
             </div>
-
           )}
-
         </Droppable>
       </DragDropContext>
     </Container>
