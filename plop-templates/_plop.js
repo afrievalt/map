3896b10ef10452cc93fbd44641aa365fa51d4161
Andy Frievalt <afrievalt@yahoo.com>
2021-03-12 07:19:29 -0600

    plop wip

--- plop-templates/_plop.js
+++ plop-templates/_plop.js
@@ -5,39 +5,61 @@ module.exports = {
       type: 'input',
       name: 'name',
       message: 'plop name?'
+    },
+    {
+      type: 'list',
+      name: 'type',
+      message: 'What kind of template?',
+      choices: ['file', 'folder']
     }
   ],
-  actions: [
-    {
-      type: 'add',
-      path: 'plop-templates/_{{name}}.js',
-      templateFile: 'plop-templates/plop-generator.hbs'
-    },
-    {
-      type: 'add',
-      path: 'plop-templates/{{name}}.js.hbs',
-      templateFile: 'plop-templates/plop-template.hbs'
-    },
-    {
-      type: 'add',
-      path: 'plop-templates/{{name}}.spec.js.hbs',
-      templateFile: 'plop-templates/plop-template.spec.hbs'
-    },
-    {
+  actions: userInput => {
+    const { type } = userInput || {}
+    const typeResults = (type === 'file')
+      ? [
+          {
+            type: 'add',
+            path: 'plop-templates/_{{name}}.js',
+            templateFile: 'plop-templates/plop-generator_file.hbs'
+          },
+          {
+            type: 'add',
+            path: 'plop-templates/{{name}}.js.hbs',
+            data: { promptName: '{{name}}' },
+            templateFile: 'plop-templates/plop-template.hbs'
+          },
+          {
+            type: 'add',
+            path: 'plop-templates/{{name}}.spec.js.hbs',
+            templateFile: 'plop-templates/plop-template.spec.hbs'
+          }
+        ]
+      : [ // type !== 'file
+          {
+            type: 'add',
+            path: 'plop-templates/_{{name}}.js',
+            templateFile: 'plop-templates/plop-generator_folder.hbs'
+          }
+        ]
+
+    return [
+      ...typeResults,
+      {
       // Action type 'append' injects a template into an existing file
-      type: 'append',
-      path: 'plopfile.js',
-      // Pattern tells plop where in the file to inject the template
-      pattern: 'const _plop = require(\'./plop-templates/_plop\')',
-      template: 'const _{{name}} = require(\'./plop-templates/_{{name}}\')'
-    },
-    {
+        type: 'append',
+        path: 'plopfile.js',
+        // Pattern tells plop where in the file to inject the template
+        pattern: 'const _plop = require(\'./plop-templates/_plop\')',
+        template: 'const _{{name}} = require(\'./plop-templates/_{{name}}\')'
+      },
+      {
       // Action type 'append' injects a template into an existing file
-      type: 'append',
-      path: 'plopfile.js',
-      // Pattern tells plop where in the file to inject the template
-      pattern: '// controller generator',
-      template: "plop.setGenerator('{{name}}', _{{name}})"
-    }
-  ]
+        type: 'append',
+        path: 'plopfile.js',
+        // Pattern tells plop where in the file to inject the template
+        pattern: '// controller generator',
+        template: "plop.setGenerator('{{name}}', _{{name}})"
+      }
+    ]
+  }
 }
