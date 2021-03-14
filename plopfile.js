a76496bee204dae70e80c556c810a6e354abf214
Andy Frievalt <afrievalt@yahoo.com>
2021-03-14 07:32:34 -0500

    plop - add base screen

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,5 @@
 const _plop = require('./plop-templates/_plop')
+const _screen = require('./plop-templates/_screen')
 const _page = require('./plop-templates/_page')
 const _field = require('./plop-templates/_field')
 const _duck = require('./plop-templates/_duck')
@@ -6,6 +7,7 @@ const _list = require('./plop-templates/_list')
 
 module.exports = function (plop) {
   // controller generator
+  plop.setGenerator('screen', _screen)
   plop.setGenerator('page', _page)
   plop.setGenerator('plop', _plop)
   plop.setGenerator('field', _field)
