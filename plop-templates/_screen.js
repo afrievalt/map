367b3cb7bba338d03e8744da8eb7051037af37f7
Andy Frievalt <afrievalt@yahoo.com>
2021-03-14 08:19:21 -0500

    feat(plop): add screen fragments

--- plop-templates/_screen.js
+++ plop-templates/_screen.js
@@ -12,8 +12,46 @@ module.exports = {
       {
         type: 'addMany',
         destination: './',
-        base: 'plop-templates/-templates/',
-        templateFiles: 'plop-templates/-templates/*.hbs'
+        base: 'plop-templates/screen-templates/',
+        templateFiles: 'plop-templates/screen-templates/*.hbs'
+      },
+      {
+        type: 'modify',
+        path: 'src/routes/routePaths.js',
+        pattern: "HOME: '/',",
+        templateFile: 'plop-templates/screen-templates/_fragments/routePaths.js.hbs'
+      },
+      {
+        type: 'modify',
+        path: 'src/routes/LeftDrawer.jsx',
+        pattern: '</List>',
+        templateFile: 'plop-templates/screen-templates/_fragments/LeftDrawer.jsx.hbs'
+      },
+      {
+        type: 'modify',
+        path: 'src/routes/MainContent.jsx',
+        pattern: '</>',
+        templateFile: 'plop-templates/screen-templates/_fragments/MainContent.jsx.hbs'
+
+      },
+      {
+        type: 'append',
+        path: 'src/routes/MainContent.jsx',
+        pattern: "import Home from '../pages/Home'",
+        template: "import {{name}} from '../pages/{{lowerCase name}}'"
+      },
+      {
+        type: 'modify',
+        path: 'src/routes/Title.jsx',
+        pattern: "HOME: 'Home',",
+        templateFile: 'plop-templates/screen-templates/_fragments/Title.jsx.hbs'
+
+      },
+      {
+        type: 'modify',
+        path: 'src/routes/FirebaseListener.jsx',
+        pattern: 'useFirebaseConnect([',
+        templateFile: 'plop-templates/screen-templates/_fragments/FirebaseListener.jsx.hbs'
       }
     ]
     return actions
