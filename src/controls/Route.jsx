acabce0a0f42d2fa4117fbb624648efbdbc75077
Andy Frievalt <afrievalt@yahoo.com>
2021-03-20 06:42:14 -0500

    feat(route): add exact

--- src/controls/Route.jsx
+++ src/controls/Route.jsx
@@ -1,10 +1,30 @@
 import { useSelector } from 'react-redux'
 import { makeArray } from '../utility/convert'
+import PropTypes from 'prop-types'
 
-const Route = ({ type, children }) => {
+const propTypes = {
+  fieldId: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  variant: PropTypes.string
+}
+const Route = ({ type, children, exact = false }) => {
   const locationType = useSelector(state => state.location.type)
-  const hasPath = !!~makeArray(type).findIndex(o => o === locationType)
+  const hasPath = !!~makeArray(type)
+    .findIndex(
+      o => exact ? o === locationType : o.startsWith(locationType)
+    )
   return hasPath ? children : null
 }
 
+Route.propTypes = propTypes
+
 export default Route
+// <Route type='FOO'>
+//  <Foo/>
+// </Route>
+// when no exact: will display url id starts with FOO. (ex: FOO_ADD, FOO_EDIT)
+
+// <Route type=['BAR, BOO'] exact>
+//   <Foo/>
+// </Route>
+// when type is array: will display url id of BAR and BOO only
