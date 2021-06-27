c65564e26d118ad95f715189a6d423589486612b
afrievalt <afrievalt@yahoo.com>
2021-06-27 16:38:45 -0500

    Delete .secretes.js

--- src/.secretes.js
+++
@@ -1,3 +0,0 @@
-// normally this file would not be publicly viewable or checked in to version control;
-export const mapboxglAccessToken = 'pk.eyJ1IjoiYWZyaWV2YWx0IiwiYSI6ImNrcHp0MWF6djAzOWYycW80ODR2MHdlN2kifQ.VCne9A82z8X0QfaUqp59NA'
-export const openChangeMapKey = 'c37feaa6-2d65-4697-bfbe-bbf4fa858c31'
